d36ce3bf34bf64abf44aac355401e3a77325dd1c
mrima <mrimamss@gmail.com>
2022-07-18 06:45:00 +0300

    add fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -28,6 +28,7 @@ class RedactingFormatter(logging.Formatter):
 
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """filters values in incoming log records"""
