e1e080bb8c5e62c5bc55a47083f67a47050a6de1
mrima <mrimamss@gmail.com>
2022-07-18 06:14:29 +0300

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+""" this module contains functions to encryot and obfuscate data """
 import re
 from typing import List
 
@@ -12,6 +12,7 @@ def filter_datum(
     """
     tmp = message
     for f in fields:
-        tmp = re.sub(f + "=.*?" + separator, f + "=" + redaction + separator, tmp)
+        tmp = re.sub(f +
+                     "=.*?" + separator, f + "=" + redaction + separator, tmp)
 
     return tmp
