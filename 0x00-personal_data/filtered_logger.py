d0c9f1898e3192221e9e05a043270d679cdc90f4
mrima <mrimamss@gmail.com>
2022-07-18 06:11:46 +0300

    filter

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import re
+from typing import List
+
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """
+    this function obfuscicates a log message
+    """
+    tmp = message
+    for f in fields:
+        tmp = re.sub(f + "=.*?" + separator, f + "=" + redaction + separator, tmp)
+
+    return tmp
